97c701c84ca218ef85bb60d2968aeaaeda5af32f
arpit57 <arpit.singhal57@gmail.com>
2024-03-28 16:59:56 +0530

    added testChroma.ipynb for testing chromadb

--- test2.py
+++ test2.py
@@ -3,7 +3,10 @@ from dotenv import load_dotenv
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain.prompts import PromptTemplate
 from langchain.chains import LLMChain
-from chromadb import ChromaDB
+# from chromadb import ChromaDB
+import chromadb
+
+
 
 load_dotenv()
 GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
@@ -15,7 +18,7 @@ prompt = PromptTemplate.from_template("You are a content creator. Write a techni
 chain = LLMChain(llm=llm, prompt=prompt, verbose=True)
 
 # Initialize ChromaDB
-db = ChromaDB("personal_text_db")
+db = chromadb.ChromaDB("personal_text_db")
 
 if __name__ == "__main__":
     topic = "vector databases"
